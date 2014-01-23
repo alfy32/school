c3c665c77e40cdcd4a4ba6cbfeb05cd7b0090cdc
Alan Christensen <alfy32@gmail.com>
2014-01-22 17:06:45 -0700

    adding server and index file to serve games

---
+++ cs-5410/server/app.js
@@ -0,0 +1,29 @@
+/* jshint node:true */
+'use strict';
+
+var express = require('express');
+var http    = require('http');
+
+var app = express();
+
+app.set('port', process.argv[2] || 3000);
+
+// all environments
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('Type secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static('../'));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port')
+    + ' in environment ' + app.get('env'));
+});
