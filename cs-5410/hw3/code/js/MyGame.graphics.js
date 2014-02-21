7fbc48ef3fd4e61ac43ed12727839c332ebb8aa8
Alan Christensen <alfy32@gmail.com>
2014-02-20 19:00:06 -0700

    in the middle of it

--- cs-5410/hw3/code/js/MyGame.graphics.js
+++ cs-5410/hw3/code/js/MyGame.graphics.js
@@ -13,7 +13,148 @@ MYGAME.graphics = (function() {
     context.restore();
   }
 
+  function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  }
+
+  /*
+  * Required Spec info:
+  *  - image
+  *  - center
+  *  - width
+  *  - rotation
+  *  - moveRate
+  *  - rotateRate
+  */
+  function Texture(spec) {
+    var that = {};
+
+    that.clicked = function(x, y) {
+      return x > spec.center.x - spec.width/2 &&
+             x < spec.center.x + spec.width/2 &&
+             y > spec.center.y - spec.height/2 &&
+             y < spec.center.y + spec.height/2;
+    };
+
+    that.offScreen = function() {
+      return spec.center.y + spec.height/2 > canvas.height;
+    };
+
+    that.rotateRight = function(elapsedTime) {
+      spec.rotation += spec.rotateRate * (elapsedTime / 1000);
+    };
+
+    that.rotateLeft = function(elapsedTime) {
+      spec.rotation -= spec.rotateRate * (elapsedTime / 1000);
+    };
+
+    that.moveLeft = function(elapsedTime) {
+      spec.center.x -= spec.moveRate * (elapsedTime / 1000);
+    };
+
+    that.moveRight = function(elapsedTime) {
+      spec.center.x += spec.moveRate * (elapsedTime / 1000);
+    };
+
+    that.moveUp = function(elapsedTime) {
+      spec.center.y -= spec.moveRate * (elapsedTime / 1000);
+    };
+
+    that.moveDown = function(elapsedTime) {
+      spec.center.y += spec.moveRate * (elapsedTime / 1000);
+    };
+
+    that.moveTo = function(center) {
+      spec.center = center;
+    };
+
+    that.draw = function() {
+      context.save();
+
+      context.translate(spec.center.x, spec.center.y);
+      context.rotate(spec.rotation);
+      context.translate(-spec.center.x, -spec.center.y);
+
+      context.drawImage(
+        spec.image,
+        spec.center.x - spec.width/2,
+        spec.center.y - spec.height/2,
+        spec.width, spec.height);
+
+      context.restore();
+    };
+
+    return that;
+  }
+
+  function getRandomLocation(width, height) {
+    width += 10; // add padding on the sides.
+    return {
+      x: Math.random() * (window.innerWidth) ,
+      y: 10//-height/2
+    };
+  }
+
+  function getSpec(image, height, width) {
+    return {
+      image: image,
+      center: getRandomLocation(width,height),
+      width: width,
+      height: height,
+      rotation: 0,
+      moveRate: 20,
+      rotateRate: 0
+    };
+  }
+
+  function canadian() {
+    var image = new Image();
+    image.src = 'img/Coin-Canadian-Dollar.png';
+    var width = 30;
+    var height = 15;
+
+    return MYGAME.graphics.Texture(getSpec(image, height, width));
+  }
+
+  function roman() {
+    var image = new Image();
+    image.src = 'img/Coin-Roman.png';
+    var width = 30;
+    var height = 15;
+
+    return MYGAME.graphics.Texture(getSpec(image, height, width));
+  }
+
+  function US() {
+    var image = new Image();
+    image.src = 'img/Coin-US-Dollary.png';
+    var width = 30;
+    var height = 15;
+
+    return MYGAME.graphics.Texture(getSpec(image, height, width));
+  }
+
+  function clock() {
+    var image = new Image();
+    image.src = 'img/Clock.png';
+    var width = 30;
+    var height = 15;
+
+    return MYGAME.graphics.Texture(getSpec(image, height, width));
+  }
+
+  var objects = {
+    canadian: canadian,
+    roman: roman,
+    US: US,
+    clock: clock
+  };
+
   return {
-    clear : clear
+    clear: clear,
+    resize: resize,
+    Texture: Texture,
+    objects: objects
   };
 }());
--- cs-5410/hw3/code/js/MyGame.initialize.js
+++ cs-5410/hw3/code/js/MyGame.initialize.js
@@ -1,4 +1,4 @@
-/*global MYGAME */
+/*global MYGAME, $, performance, requestAnimationFrame */
 
 MYGAME.initialize = function initialize() {
   'use strict';
@@ -8,30 +8,96 @@ MYGAME.initialize = function initialize() {
   var myTouch = MYGAME.input.Touch();
 
   MYGAME.lastTimeStamp = performance.now();
+  var money = 0;
+
+  window.onresize = MYGAME.graphics.resize;
+
+  var objects = [];
+  var addObjectRate = 3000;
+  var timeSinceLastAddObject = 0;
+
+  function addObject() {
+    var randomObject = Math.floor(Math.random() * 4);
+
+    console.log(randomObject);
+
+    if(randomObject === 0) randomObject = 'US';
+    else if(randomObject === 1) randomObject = 'roman';
+    else if(randomObject === 2) randomObject = 'canadian';
+    else if(randomObject === 3) randomObject = 'clock';
+
+    randomObject = MYGAME.graphics.objects[randomObject]();
+
+    myMouse.registerCommand('mousedown', function (e){
+      if(randomObject.clicked(e.clientX, e.clientY)) {
+        explodeObject(randomObject);
+      }
+    });
+
+    objects.push(randomObject);
+  }
+
+  function explodeObject(obj) {
+    console.log('clicked');
+    removeObject(obj);
+  }
+
+  function removeObject(obj) {
+    console.log('remove')
+    var index = objects.indexOf(obj);
+    objects.splice(index, 1);
+  }
 
   //------------------------------------------------------------------
   //
   // This is the Game Loop function!
   //
   //------------------------------------------------------------------
+  var MAX_FRAME_RATE = 1000/30;
+
   function gameLoop(time) {
     MYGAME.elapsedTime = time - MYGAME.lastTimeStamp;
     MYGAME.lastTimeStamp = time;
 
-    update();
+    update(MYGAME.elapsedTime);
     render();
 
-    requestAnimationFrame(gameLoop);
-  };
+    setTimeout(function() {
+      requestAnimationFrame(gameLoop);
+    }, MAX_FRAME_RATE - MYGAME.elapsedTime);
+  }
 
-  function update() {
-    myKeyboard.update(MYGAME.elapsedTime);
-    myMouse.update(MYGAME.elapsedTime);
-    myTouch.update(MYGAME.elapsedTime);
+  function update(elapsedTime) {
+    myKeyboard.update(elapsedTime);
+    myMouse.update(elapsedTime);
+    myTouch.update(elapsedTime);
+
+    timeSinceLastAddObject += elapsedTime;
+
+    // console.log(timeSinceLastAddObject, addObjectRate);
+
+    if(timeSinceLastAddObject > addObjectRate) {
+      timeSinceLastAddObject -= addObjectRate;
+      addObject();
+    }
+
+    for(var i in objects) {
+      if(objects[i].offScreen()) removeObject(objects[i]);
+      else {
+        objects[i].moveDown(elapsedTime);
+        objects[i].draw();
+      }
+    }
+
+    $('.money').text(Math.floor(timeSinceLastAddObject));
   }
 
   function render() {
-    MYGAME.graphics.clear();;
+    MYGAME.graphics.clear();
+
+    for(var i in objects) {
+      objects[i].draw();
+    }
   }
 
   requestAnimationFrame(gameLoop);
