56657bd953550304969d9c0b8ccefbddd55b0b40
Alan Christensen <alfy32@gmail.com>
2014-03-03 21:22:45 -0700

    starting to make more

---
+++ cs-5200/hw3/project/BrilliantStudent/Agent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AgentCommon;
+using Messages;
+using Common;
+
+namespace BrilliantStudent
+{
+    class Agent
+    {
+        static void Main(string[] args)
+        {
+            String address = "localhost";
+            int port = 23456;
+
+            if (args.Length == 2)
+            {
+                address = args[0];
+                port = Convert.ToInt32(args[1]);
+            }
+
+            Communicator communicator = new Communicator(port);
+
+            Listener listener = new Listener(communicator);
+            Doer doer = new Doer(communicator);
+
+            listener.Start();
+            doer.Start();
+
+            Console.WriteLine("Agent Started: Listening on port " + port);
+            Console.WriteLine();
+
+            MessageQueue messageQueue = RequestMessageQueue.getQueue();
+
+            Console.WriteLine("Hit any key to quit...");
+
+            Console.ReadKey(false);
+
+            listener.Stop();
+            doer.Stop();
+        }
+    }
+}
