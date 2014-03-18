8b5194378c5a75dbc810596f2122160720a16986
Alan Christensen <alfy32@gmail.com>
2014-03-18 15:54:27 -0600

    Initial commit

---
+++ cs-5050/hw4/code/FFT.cpp
@@ -0,0 +1,5 @@
+#include "FFT.hpp"
+
+FFT::FFT() {
+
+}
---
+++ cs-5050/hw4/code/FFT.hpp
@@ -0,0 +1,16 @@
+#ifndef FFT_HPP
+#define FFT_HPP
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <utility>
+
+class FFT {
+private:
+
+public:
+  FFT();
+};
+
+#endif
